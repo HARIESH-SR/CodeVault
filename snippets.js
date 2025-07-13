d0ed9280df8c41ead890f1e211478e0bf2ffa8ad
Hariesh S R <121657436+HARIESH-SR@users.noreply.github.com>
2025-07-13 19:32:49 +0530

    Update snippets.js

--- snippets.js
+++ snippets.js
@@ -12,9 +12,7 @@ for _ in range(TestCases):
     output_to_print = functionName(enter_the_args)  # Replace with function name and pass the input
 
     print(output_to_print)
-
-
-    `.trim(),
+`.trim(),
 
     cpp: `
 #include <iostream>
@@ -37,7 +35,7 @@ int main() {
     }
     return 0;
 }
-    `.trim(),
+`.trim(),
 
     java: `
 import java.util.*;
@@ -56,10 +54,10 @@ public class Main {
             System.out.println(output_to_print);
         }
     }
-    
+
     //Type your function here
 }
-    `.trim(),
+`.trim(),
 
     c: `
 #include <stdio.h>
@@ -73,30 +71,47 @@ int main() {
 
         // Type your code to get input here
 
-        
         printf("%d\\n", output_to_print); // Replace with your function and arguments
     }
     return 0;
 }
-    `.trim()
+`.trim()
   };
 
   const supportedLangs = Object.keys(snippets);
+
   supportedLangs.forEach(lang => {
     monaco.languages.registerCompletionItemProvider(lang, {
-      triggerCharacters: ["l"], // typing "l" will trigger
-      provideCompletionItems: () => {
-        return {
-          suggestions: [
-            {
-              label: "leetcodefunction",
-              kind: monaco.languages.CompletionItemKind.Snippet,
-              insertText: snippets[lang],
-              insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-              documentation: "LeetCode-style test case function runner"
-            }
-          ]
-        };
+      triggerCharacters: [],
+      provideCompletionItems: (model, position) => {
+        try {
+          const word = model.getWordUntilPosition(position).word;
+          if (!'leetcodefunction'.startsWith(word)) return { suggestions: [] };
+
+          const range = {
+            startLineNumber: position.lineNumber,
+            endLineNumber: position.lineNumber,
+            startColumn: position.column - word.length,
+            endColumn: position.column,
+          };
+
+          return {
+            suggestions: [
+              {
+                label: "leetcodefunction",
+                kind: monaco.languages.CompletionItemKind.Snippet,
+                insertText: snippets[lang],
+                insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                documentation: "LeetCode-style test case function runner",
+                range,
+                sortText: "zzz"
+              }
+            ]
+          };
+        } catch (e) {
+          if (e.name !== "Canceled") console.error(e);
+          return { suggestions: [] };
+        }
       }
     });
   });
